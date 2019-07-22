9f8606637d00cf83db73f63207f0e8706cd1978e
amichaiman <ami.dan.man@gmail.com>
2019-07-22 11:40:27 +0300

    in marathon

--- 4.c
+++ 4.c
@@ -4,32 +4,46 @@
 #define N (3)
 #define M (5)
 
-/* a */
-int* max_rows_cols(int a[][M], int **max_cols) {
-    int i, j;
-    int *max_rows = (int*) calloc(N, sizeof(int));
-    *max_cols = (int*) calloc(M, sizeof(int));
-
-    /* fill max_rows */
-    for (i=0; i<N; i++) {
-        for (j=0; j<M; j++) {
-            if (a[i][j] > a[i][max_rows[i]]) {
-                max_rows[i] = j;
-            }
+int get_max_in_row(int* a) {
+    int i, index_max = 0;
+    for (i=1; i<M; i++) {
+        if (a[i] > a[index_max]) {
+            index_max = i;
         }
     }
+    return index_max;
+}
 
-    /* fill max_cols */
-    for (i=0; i<M; i++) {
-        for (j=0; j<N; j++) {
-            if (a[j][i] > a[(*max_cols)[i]][i]) {
-                (*max_cols)[i] = j;
-            }
+int get_max_in_col(int a[][M], int col) {
+    int i, index_max = 0;
+
+    for (i=1; i<N; i++) {
+        if (a[i][col] > a[index_max][col]) {
+            index_max = i;
         }
+    }
+    return index_max;
+}
+
+/* a */
+// max_rows {2, 0, 1}        //returns from function
+// max_cols {1, 2, 0, 1, 2}  //returns from output param
+int* max_rows_cols(int a[][M], int** p_max_cols) {
+    int i;
+    int* max_rows = (int*) malloc (N*sizeof(int));
+    *p_max_cols   = (int*) malloc (M*sizeof(int));
+
+    /* fill max_row array */
+    for (i=0; i<N; i++) {
+        max_rows[i] = get_max_in_row(a[i]);
+    }
+
+    /* fill max_col array */
+    for (i=0; i<M; i++) {
+        (*p_max_cols)[i] = get_max_in_col(a, i);
     }
     return max_rows;
 }
-
 
 /* b */
 void print_array(int *a, int size) {
